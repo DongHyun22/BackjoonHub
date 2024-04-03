33138c167735608cc3d23583cf30eb378669128d
김동현 <79788971+DongHyun22@users.noreply.github.com>
2024-04-03 19:49:52 +0900

    [level 2] Title: 가장 큰 수, Time: 2.51 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
+        String[] s = new String[numbers.length];
+        
+        for(int i = 0; i < numbers.length; i++) {
+            s[i] = String.valueOf(numbers[i]);
+        }
+        
+        Arrays.sort(s, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if(s[0].equals("0")) {
+            return "0";
+        }
+        
+        for(int i = 0; i < s.length; i++) {
+            sb.append(s[i]);
+        }
+        return sb.toString();
+    }
+}
