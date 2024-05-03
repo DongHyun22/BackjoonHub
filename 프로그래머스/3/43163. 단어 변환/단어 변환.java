804b3079d98108081198881ff7a334b92f1dd9e1
김동현 <79788971+DongHyun22@users.noreply.github.com>
2024-05-03 13:54:35 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,49 @@
+
+class Solution {
+    static String target;
+    static String[] words;
+    static int min = Integer.MAX_VALUE;
+    public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        this.target = target;
+        this.words = words.clone();
+        dfs(begin, 0, new boolean[words.length]);
+        if(min == Integer.MAX_VALUE) {
+            answer = 0;
+        }
+        else {
+            answer = min;
+        }
+        return answer;
+    }
+    public void dfs(String s, int count, boolean[] visited) {
+        if(s.equals(target) && count < min) {
+            min = count;
+            return;
+        }
+        
+        for(int i = 0; i < words.length; i++) {
+            if(!visited[i] && check(s, words[i])) {
+                visited[i] = true;
+                dfs(words[i], count + 1, visited);
+                visited[i] = false;
+            }
+        }
+        
+    }
+    
+    // 한 개의 알파벳만 바꿀 수 있는지 판단하는 함수
+    public boolean check(String s, String w) {
+        char[] s_arr = s.toCharArray();
+        char[] w_arr = w.toCharArray();
+        boolean flag = false;
+        for(int i = 0; i < s.length(); i++) {
+            if(s_arr[i] != w_arr[i]) {
+                if(flag)
+                    return false;
+                flag = true;
+            }
+        }
+        return flag;
+    }
+}
